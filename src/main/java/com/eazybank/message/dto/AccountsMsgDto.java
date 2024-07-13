a33652c5ec43e6809b6ba6aac1dd98f48c90ae8d
ravsingh15 <ravinderskohli@gmail.com>
2024-07-13 20:51:28 +0530

    add apache kafka module

---
+++ src/main/java/com/eazybank/message/dto/AccountsMsgDto.java
@@ -0,0 +1,4 @@
+package com.eazybank.message.dto;
+
+public record AccountsMsgDto(Long accountNumber,String name, String email, String mobileNumber) {
+}
---
+++ src/test/java/com/eazybank/message/MessageApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.eazybank.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
